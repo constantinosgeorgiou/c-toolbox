80ac5bb79f8e027457aa0fc785b2434b037b7c72
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-04-29 20:59:15 +0300

    Add tests for find and find_node functions.

--- tests/ADTBidirectionalList_test.c
+++ tests/ADTBidirectionalList_test.c
@@ -9,6 +9,12 @@
 
 #include "acutest.h"  // Απλή βιβλιοθήκη για unit testing
 
+/// @brief Compares two int*.
+///
+/// @return < 0, if a < b, or, > 0, if b < a, or, 0, if a and b are equivalent
+///
+static int compare_ints(const void* a, const void* b) { return *(int*)a - *(int*)b; }
+
 /// @brief Allocates memory for an integer with given value.
 ///
 /// @return Newly created pointer to integer.
@@ -124,10 +130,60 @@ void test_remove(void) {
     blist_destroy(blist);
 }
 
+void test_find() {
+    BList blist = blist_create(NULL);
+    int N = 1000;
+    int array[N];
+
+    for (int i = 0; i < N; i++) {
+        array[i] = i;
+        blist_insert(blist, BLIST_BOF, &array[i]);
+    }
+
+    // Find all elements:
+    for (int i = 0; i < N; i++) {
+        int* value = blist_find(blist, &i, compare_ints);
+        TEST_CHECK(value == &array[i]);
+    }
+
+    // Find a value that is not part of the bidirectional list:
+    int not_exists = -1;
+    TEST_CHECK(blist_find(blist, &not_exists, compare_ints) == NULL);
+
+    blist_destroy(blist);
+}
+
+void test_find_node() {
+    BList blist = blist_create(NULL);
+
+    int N = 1000;
+    int array[N];
+
+    for (int i = 0; i < N; i++) {
+        array[i] = i;
+        blist_insert(blist, BLIST_BOF, &array[i]);
+    }
+
+    BListNode node = blist_first(blist);
+
+    for (int i = N - 1; i >= 0; i--) {
+        BListNode found_node = blist_find_node(blist, &i, compare_ints);
+
+        TEST_CHECK(found_node == node);
+        TEST_CHECK(blist_node_value(blist, found_node) == &array[i]);
+
+        node = blist_next(blist, node);
+    }
+
+    blist_destroy(blist);
+}
+
 TEST_LIST = {
     {"create", test_create},
     {"insert", test_insert},
     {"remove", test_remove},
+    {"find", test_find},
+    {"find_node", test_find_node},
 
     {NULL, NULL}  // End of tests.
 };
