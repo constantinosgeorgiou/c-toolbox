d7de6083c25b6c0636037f16080a5b815ae4c67c
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-04-29 20:50:34 +0300

    Add test for remove function.

--- tests/ADTBidirectionalList_test.c
+++ tests/ADTBidirectionalList_test.c
@@ -32,7 +32,7 @@ void test_create(void) {
 }
 
 void test_insert(void) {
-    int N = 10;
+    int N = 1000;
 
     // Insert at the beginning.
     BList blist = blist_create(free);
@@ -65,9 +65,69 @@ void test_insert(void) {
     blist_destroy(blist);
 }
 
+void test_remove(void) {
+    BList blist = blist_create(free);
+
+    int N = 10;
+
+    // Insert at the beginning. Remove from the beginning.
+    for (int i = 0; i < N; i++) {
+        blist_insert(blist, BLIST_BOF, create_int(i));
+    }
+    for (int i = N - 1; i >= 0; i--) {
+        TEST_CHECK(*(int*)blist_node_value(blist, blist_first(blist)) == i);
+        blist_remove(blist, blist_first(blist));
+    }
+    TEST_CHECK(blist_size(blist) == 0);
+
+    // Insert at the beginning. Remove from the end.
+    for (int i = 0; i < N; i++) {
+        blist_insert(blist, BLIST_BOF, create_int(i));
+    }
+    for (int i = 0; i < N; i++) {
+        TEST_CHECK(*(int*)blist_node_value(blist, blist_last(blist)) == i);
+        blist_remove(blist, blist_last(blist));
+    }
+    TEST_CHECK(blist_size(blist) == 0);
+
+    // Insert at the end. Remove from the beginning.
+    for (int i = 0; i < N; i++) {
+        blist_insert(blist, blist_last(blist), create_int(i));
+    }
+    for (int i = 0; i < N; i++) {
+        TEST_CHECK(*(int*)blist_node_value(blist, blist_first(blist)) == i);
+        blist_remove(blist, blist_first(blist));
+    }
+    TEST_CHECK(blist_size(blist) == 0);
+
+    // Insert at the end. Remove from the end.
+    for (int i = 0; i < N; i++) {
+        blist_insert(blist, blist_last(blist), create_int(i));
+    }
+    for (int i = N - 1; i >= 0; i--) {
+        TEST_CHECK(*(int*)blist_node_value(blist, blist_last(blist)) == i);
+        blist_remove(blist, blist_last(blist));
+    }
+    TEST_CHECK(blist_size(blist) == 0);
+
+    // Remove from the middle.
+    for (int i = 0; i < N; i++) {
+        blist_insert(blist, blist_last(blist), create_int(i));
+    }
+    BListNode middle = blist_first(blist);
+    for (int i = 0; i < N / 2; i++) {
+        middle = blist_next(blist, middle);
+    }
+    blist_remove(blist, middle);
+    TEST_CHECK(blist_size(blist) == N - 1);
+
+    blist_destroy(blist);
+}
+
 TEST_LIST = {
     {"create", test_create},
     {"insert", test_insert},
+    {"remove", test_remove},
 
     {NULL, NULL}  // End of tests.
 };
