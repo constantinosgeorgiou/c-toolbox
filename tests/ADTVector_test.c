c3aedb77b87080e4171fe06456b1eee6d7c725cd
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-07-01 01:57:09 +0300

    Add test_insert.

--- tests/ADTVector_test.c
+++ tests/ADTVector_test.c
@@ -21,8 +21,28 @@ void test_create(void) {
     vector_destroy(vec2);
 }
 
+void test_insert(void) {
+    Vector vec = vector_create(0, NULL);
+    int N = 1000;
+    int* array = malloc(N * sizeof(*array));
+
+    // Insert 1000 elements to cause multiple resizes.
+    for (int i = 0; i < 1000; i++) {
+        vector_insert_last(vec, &array[i]);
+        TEST_CHECK(vector_size(vec) == i + 1);
+        TEST_CHECK(vector_get_at(vec, i) == &array[i]);
+    }
+
+    for (int i = 0; i < 1000; i++)
+        TEST_CHECK(vector_get_at(vec, i) == &array[i]);
+
+    vector_destroy(vec);
+    free(array);
+}
+
 TEST_LIST = {
     {"vector_create", test_create},
+    {"vector_insert", test_insert},
 
     {NULL, NULL}  // End of tests.
 };
