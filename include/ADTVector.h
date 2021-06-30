59a99e63849f373db097f34971dd61baa74f6c15
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-07-01 01:08:09 +0300

    Restructure and remove common.h dependency.

--- include/ADTVector.h
+++ include/ADTVector.h
@@ -1,34 +1,39 @@
-///////////////////////////////////////////////////////////////////
-//
-// ADT Vector
-//
-// Abstract "array" μεταβλητού μεγέθους. Παρέχει πρόσβαση σε
-// οποιοδήποτε στοιχείο (μέσω της θέσης του), και προσθήκη/αφαίρεση
-// στοιχείων στο τέλος του vector.
-//
-///////////////////////////////////////////////////////////////////
+#pragma once
 
-#pragma once // #include το πολύ μία φορά
+#ifndef COMMON_FUNCTIONS
+#define COMMON_FUNCTIONS
 
-#include "common_types.h"
+/// @brief Pointer to function that compares elements a and b.
+///
+/// @return < 0, if a < b, or, > 0, if b < a, or, 0, if a and b are equivalent
+///
+typedef int (*CompareFunc)(const void* a, const void* b);
 
-#define VECTOR_FAIL	(Vector)0
+/// @brief Pointer to function that destroys a value.
+///
+typedef void (*DestroyFunc)(void* value);
 
+#endif
 
-// Ένα vector αναπαριστάται από τον τύπο Vector. Ο χρήστης δε χρειάζεται να γνωρίζει το περιεχόμενο
-// του τύπου αυτού, απλά χρησιμοποιεί τις συναρτήσεις vector_<foo> που δέχονται και επιστρέφουν Vector.
-//
-// Ο τύπος Vector ορίζεται ως pointer στο "struct vector" του οποίου το περιεχόμενο είναι άγνωστο
-// (incomplete struct), και εξαρτάται από την υλοποίηση του ADT Vector.
-//
+#define VECTOR_ERROR (Vector)0
+
 typedef struct vector* Vector;
-
 
 // Δημιουργεί και επιστρέφει ένα νεό vector μεγέθους size, με στοιχεία αρχικοποιημένα σε NULL.
 // Αν δεν υπάρχει διαθέσιμη μνήμη επιστρέφει VECTOR_FAIL.
 // Αν destroy_value != NULL, τότε καλείται destroy_value(value) κάθε φορά που αφαιρείται (ή αντικαθίσταται) ένα στοιχείο.
 
 Vector vector_create(int size, DestroyFunc destroy_value);
+
+// Ελευθερώνει όλη τη μνήμη που δεσμεύει το vector vec.
+// Οποιαδήποτε λειτουργία πάνω στο vector μετά το destroy είναι μη ορισμένη.
+
+void vector_destroy(Vector vec);
+
+// Αλλάζει τη συνάρτηση που καλείται σε κάθε αφαίρεση/αντικατάσταση στοιχείου σε
+// destroy_value. Επιστρέφει την προηγούμενη τιμή της συνάρτησης.
+
+DestroyFunc vector_set_destroy_value(Vector vec, DestroyFunc destroy_value);
 
 // Επιστρέφει τον αριθμό στοιχείων που περιέχει το vector vec.
 
@@ -44,6 +49,11 @@ void vector_insert_last(Vector vec, void* value);
 
 void vector_remove_last(Vector vec);
 
+// Βρίσκει και επιστρέφει το πρώτο στοιχείο στο vector που να είναι ίσο με value
+// (με βάση τη συνάρτηση compare), ή NULL αν δεν βρεθεί κανένα στοιχείο.
+
+void* vector_find(Vector vec, void* value, CompareFunc compare);
+
 // Επιστρέφει την τιμή στη θέση pos του vector vec (μη ορισμένο αποτέλεσμα αν pos < 0 ή pos >= size)
 
 void* vector_get_at(Vector vec, int pos);
@@ -53,21 +63,20 @@ void* vector_get_at(Vector vec, int pos);
 
 void vector_set_at(Vector vec, int pos, void* value);
 
-// Βρίσκει και επιστρέφει το πρώτο στοιχείο στο vector που να είναι ίσο με value
-// (με βάση τη συνάρτηση compare), ή NULL αν δεν βρεθεί κανένα στοιχείο.
+// Οι σταθερές αυτές συμβολίζουν εικονικούς κόμβους _πριν_ τον πρώτο και _μετά_ τον τελευταίο
+#define VECTOR_BOF (VectorNode)0
+#define VECTOR_EOF (VectorNode)0
 
-void* vector_find(Vector vec, void* value, CompareFunc compare);
+typedef struct vector_node* VectorNode;
 
-// Αλλάζει τη συνάρτηση που καλείται σε κάθε αφαίρεση/αντικατάσταση στοιχείου σε
-// destroy_value. Επιστρέφει την προηγούμενη τιμή της συνάρτησης.
+// Βρίσκει το πρώτο στοιχείο στο vector που να είναι ίσο με value (με βάση τη συνάρτηση compare).
+// Επιστρέφει τον κόμβο του στοιχείου, ή VECTOR_EOF αν δεν βρεθεί.
 
-DestroyFunc vector_set_destroy_value(Vector vec, DestroyFunc destroy_value);
+VectorNode vector_find_node(Vector vec, void* value, CompareFunc compare);
 
-// Ελευθερώνει όλη τη μνήμη που δεσμεύει το vector vec.
-// Οποιαδήποτε λειτουργία πάνω στο vector μετά το destroy είναι μη ορισμένη.
+// Επιστρέφει το περιεχόμενο του κόμβου node
 
-void vector_destroy(Vector vec);
-
+void* vector_node_value(Vector vec, VectorNode node);
 
 // Διάσχιση του vector ////////////////////////////////////////////////////////////
 //
@@ -76,12 +85,6 @@ void vector_destroy(Vector vec);
 // εύκολα να διασχίσουμε το array και μέσω indexes. Παραμένουν πάντως χρήσιμες,
 // τόσο για ομοιομορφία με τους άλλους ADTs, αλλά και γιατί για κάποιες υλοποιήσεις
 // η διάσχιση μέσω κόμβων μπορεί να είναι πιο γρήγορη.
-
-// Οι σταθερές αυτές συμβολίζουν εικονικούς κόμβους _πριν_ τον πρώτο και _μετά_ τον τελευταίο
-#define VECTOR_BOF (VectorNode)0
-#define VECTOR_EOF (VectorNode)0
-
-typedef struct vector_node* VectorNode;
 
 // Επιστρέφουν τον πρώτο και τον τελευταίο κομβο του vector, ή VECTOR_BOF / VECTOR_EOF αντίστοιχα αν το vector είναι κενό
 
@@ -93,12 +96,3 @@ VectorNode vector_last(Vector vec);
 
 VectorNode vector_next(Vector vec, VectorNode node);
 VectorNode vector_previous(Vector vec, VectorNode node);
-
-// Επιστρέφει το περιεχόμενο του κόμβου node
-
-void* vector_node_value(Vector vec, VectorNode node);
-
-// Βρίσκει το πρώτο στοιχείο στο vector που να είναι ίσο με value (με βάση τη συνάρτηση compare).
-// Επιστρέφει τον κόμβο του στοιχείου, ή VECTOR_EOF αν δεν βρεθεί.
-
-VectorNode vector_find_node(Vector vec, void* value, CompareFunc compare);
