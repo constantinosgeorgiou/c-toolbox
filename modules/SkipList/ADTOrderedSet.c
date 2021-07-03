40da62537d16c7e8a6860de8d3677c740c0cc3b1
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-07-03 23:54:57 +0300

    oset_insert: Add implementation
    Increase capacity if equal to size
    
    Add pcg RNG
    
    oset_next: Update implementation
    
    oset_find_node: Update implementation

--- modules/SkipList/ADTOrderedSet.c
+++ modules/SkipList/ADTOrderedSet.c
@@ -2,8 +2,13 @@
 
 #include <assert.h>   // assert
 #include <stdbool.h>  // true, false
+#include <stdio.h>    // printf
 #include <stdlib.h>   // malloc, free, sizeof
 #include <string.h>
+#include <time.h>  // time
+
+#include "ADTVector.h"
+#include "pcg_basic.h"  // pcg32_srandom, pcg32_boundedrand
 
 /// @brief Levels of forward pointers a node can have.
 ///
@@ -11,6 +16,15 @@
 /// In the event the Oredered Set has 2^16 elements, double its max level.
 ///
 #define OSET_LEVELS 16
+
+/// @brief Precalculated sizes to used when oset->size needs to be doubled.
+///
+///
+static size_t sizes[] = {
+    65536,                // 2^16
+    4294967296,           // 2^32
+    18446744073709551616  // 2^64
+};
 
 struct ordered_set {
     CompareFunc compare;
@@ -21,7 +35,8 @@ struct ordered_set {
                     // Oredered Set can have 2^max_level, if that number is reached, then
                     // double the max_level.
 
-    int size;  // Number of elements in the Ordered Set.
+    size_t capacity;  // Maximum number of elements in the Ordered Set. Doubles if capacity == size.
+    size_t size;      // Number of elements in the Ordered Set.
 
     OrderedSetNode first;
     OrderedSetNode last;
@@ -40,6 +55,50 @@ struct ordered_set_node {
     void* key;
     void* value;
 };
+
+/// @brief Increases the capacity of specified Ordered Set.
+///
+static void capacity_increase(OrderedSet oset) {
+    size_t old_capacity = oset->capacity;
+
+    int sizes_len = sizeof(sizes) / sizeof(size_t);
+
+    for (int i = 0; i < sizes_len; i++) {
+        if (sizes[i] > old_capacity) {
+            oset->capacity = sizes[i];
+            break;
+        }
+    }
+}
+
+/// @brief Determines if the Random Number Generator is seeeded.
+///
+/// true, if seeded, otherwise false.
+///
+static bool seeded = false;
+
+/// @brief Seeds the Random Number Generator.
+///
+/// @param initseq Selects the output sequence for the RNG, you can pass any 64-bit value, although
+///                only the low 63 bits are significant.
+///
+static void random_seed(int initseq) {
+    pcg32_srandom(time(NULL) ^ (intptr_t)&printf, (intptr_t)&initseq);
+    seeded = true;
+}
+
+/// @brief Choose a random level between 1 and max_level.
+///
+/// @param max_level Maximum level in an Ordered Set.
+///
+static int level_random(int max_level) {
+    int level = 1;
+
+    // "Flip coins". Increase level until tails(0).
+    while (pcg32_boundedrand(2) && level < max_level) level++;
+
+    return level;
+}
 
 /// @brief Creates and returns an Ordered Set node.
 ///
@@ -86,6 +145,34 @@ static void node_destroy(OrderedSetNode node, DestroyFunc destroy_key, DestroyFu
     free(node);
 }
 
+/// @brief Finds and returns previous node of node with specified key.
+///
+/// @param update Tracks the nodes traversed to previous node. If update == NULL, traversed nodes
+///               are not tracked.
+///
+/// @return Previous node of node with specified key.
+///
+static OrderedSetNode node_find_previous(OrderedSet oset, void* key, Vector update) {
+    assert(key != NULL);
+
+    OrderedSetNode node = oset->header;
+
+    // Traverse levels from top to bottom.
+    for (int i = node->levels - 1; i >= 0; i--) {
+        // Traverse forward links in level.
+        while (node->forward[i] != OSET_EOF && oset->compare(node->forward[i]->key, key) < 0) {
+            node = node->forward[i];
+        }
+
+        // Track traversed nodes.
+        if (update != NULL) {
+            vector_set_at(update, i, node);
+        }
+    }
+
+    return node;
+}
+
 OrderedSet oset_create(CompareFunc compare, DestroyFunc destroy_key, DestroyFunc destroy_value) {
     OrderedSet oset = malloc(sizeof(*oset));
     if (oset == NULL) {
@@ -97,6 +184,7 @@ OrderedSet oset_create(CompareFunc compare, DestroyFunc destroy_key, DestroyFunc
     oset->destroy_value = destroy_value;
 
     oset->max_level = OSET_LEVELS;
+    oset->capacity = sizes[0];
     oset->size = 0;
 
     oset->first = OSET_EOF;
@@ -107,6 +195,9 @@ OrderedSet oset_create(CompareFunc compare, DestroyFunc destroy_key, DestroyFunc
     if (oset->header == NULL) {
         return OSET_ERROR;
     }
+
+    // Seed Pseudo Random Number Generator, if not yet seeded.
+    if (seeded == false) random_seed(oset->max_level);
 
     return oset;
 }
@@ -137,10 +228,45 @@ DestroyFunc oset_set_destroy_value(OrderedSet oset, DestroyFunc destroy_value) {
     return old;
 }
 
-int oset_size(OrderedSet oset) { return oset->size; }
+size_t oset_size(OrderedSet oset) { return oset->size; }
 
 void oset_insert(OrderedSet oset, void* key, void* value) {
     assert(key != NULL);
+
+    if (oset->size == oset->capacity) {
+        capacity_increase(oset);
+    }
+
+    Vector update = vector_create(oset->max_level, NULL);
+
+    OrderedSetNode target = node_find_previous(oset, key, update);
+
+    OrderedSetNode new_node = node_create(key, value, level_random(oset->max_level), false);
+
+    // Insert new_node after node.
+    for (int i = new_node->levels - 1; i >= 0; i--) {
+        OrderedSetNode node = vector_get_at(update, i);
+        new_node->forward[i] = node->forward[i];
+        node->forward[i] = new_node;
+    }
+
+    new_node->previous = target;
+
+    // Update first pointer.
+    if (oset->first == OSET_EOF || oset->compare(new_node->key, oset->first->key) < 0) {
+        oset->first = new_node;
+    }
+
+    // Update last pointer.
+    if (oset->last == OSET_EOF || oset->compare(new_node->key, oset->last->key) > 0) {
+        oset->last = new_node;
+    }
+
+    // Update size.
+    oset->size++;
+
+    // Free memory allocated for the vector.
+    vector_destroy(update);
 }
 
 bool oset_remove(OrderedSet oset, void* key) {
@@ -168,11 +294,11 @@ void oset_concat(OrderedSet a, OrderedSet b) {}
 OrderedSetNode oset_find_node(OrderedSet oset, void* key) {
     assert(key != NULL);
 
-    // OrderedSetNode node = node_find_previous(oset, key);
+    OrderedSetNode node = node_find_previous(oset, key, NULL);
 
-    // if (node->next != OSET_EOF && oset->compare(node->next->key, key) == 0) {
-    //     return node->next;
-    // }
+    if (node->forward[0] != OSET_EOF && oset->compare(node->forward[0]->key, key) == 0) {
+        return node->forward[0];
+    }
 
     return OSET_EOF;
 }
@@ -195,8 +321,7 @@ OrderedSetNode oset_last(OrderedSet oset) { return oset->last; }
 
 OrderedSetNode oset_next(OrderedSet oset, OrderedSetNode node) {
     assert(node != NULL);
-    // return node->next;
-    return NULL;
+    return node->forward[0];
 }
 
 OrderedSetNode oset_previous(OrderedSet oset, OrderedSetNode node) {
