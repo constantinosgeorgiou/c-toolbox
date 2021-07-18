c355a6279c90bdfc07f62d5ae8a31f1953e2a41e
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-07-18 22:47:04 +0300

    oset_merge: Add implementation.
    oset_insert: Fix previous pointers bug.
    
    oset_concat: Fix previous pointers bug.
    
    oset_split: Fix first and last pointers bug.
    
    oset_split: Fix previous pointers bug.
    
    oset_split: Fix decrease levels bug.

--- modules/SkipList/ADTOrderedSet.c
+++ modules/SkipList/ADTOrderedSet.c
@@ -55,6 +55,10 @@ struct ordered_set_node {
     void* value;
 };
 
+/// @brief Returns the maximum between two integers.
+///
+static int max(int a, int b) { return a > b ? a : b; }
+
 /// @brief Increases the capacity of specified Ordered Set.
 ///
 static void capacity_increase(OrderedSet oset) {
@@ -258,7 +262,9 @@ void oset_insert(OrderedSet oset, void* key, void* value) {
         node->forward[i] = new_node;
     }
 
+    // Update previous pointers.
     new_node->previous = target;
+    if (new_node->forward[0] != OSET_EOF) new_node->forward[0]->previous = new_node;
 
     // Update first pointer.
     if (oset->first == OSET_EOF || oset->compare(new_node->key, oset->first->key) < 0) {
@@ -330,42 +336,46 @@ OrderedSet oset_split(OrderedSet oset, void* split_key) {
         split->max_level = oset->max_level;  // Update max_level of split.
         // Reallocate and initialize to NULL the forward array of split header node.
         free(split->header->forward);
-        split->header->forward = calloc(split->max_level * sizeof(*split->header->forward), split->max_level);
+        split->header->forward =
+            calloc(split->max_level * sizeof(*split->header->forward), split->max_level);
         if (split->header->forward == NULL) return OSET_ERROR;
     }
     split->header->levels = oset->header->levels;
-    split->first = oset->first;
-    split->last = oset->last;
 
     OrderedSetNode node = oset->header;
 
     // Traverse levels from top to bottom.
     for (int i = node->levels - 1; i >= 0; i--) {
         // Traverse forward links in level.
-        while (node->forward[i] != OSET_EOF && oset->compare(node->forward[i]->key, split_key) <= 0) {
+        while (
+            node->forward[i] != OSET_EOF &&
+            oset->compare(node->forward[i]->key, split_key) <= 0) {
             node = node->forward[i];
         }
         split->header->forward[i] = node->forward[i];
         node->forward[i] = OSET_EOF;
-
-        if (i == 0) {
-            split->first = split->header->forward[i];
-
-            // Update first and last pointers of original Ordered Set.
-            if (oset->first == split->first) oset->first = OSET_EOF;
-            oset->last = node;
-        }
     }
+
+    // Update SPLIT previous pointers.
+    if (split->header->forward[0] != OSET_EOF) split->header->forward[0]->previous = split->header;
+
+    // Update first and last pointers of SPLIT.
+    split->first = split->header->forward[0];
+    if (split->first != OSET_EOF) split->last = oset->last;  // Atleast one element is in SPLIT.
+
+    // Update first and last pointers of OSET.
+    if (oset->first == split->first) oset->first = OSET_EOF;
+    oset->last = node;
 
     // Decrease excess levels of original Ordered Set.
     node = oset->header;
-    while (node->forward[node->levels] == OSET_EOF && node->levels > 0) {
+    while (node->forward[node->levels - 1] == OSET_EOF && node->levels - 1 > 0) {
         node->levels--;
     }
 
     // Decrease excess levels of split Ordered Set.
     node = split->header;
-    while (node->forward[node->levels] == OSET_EOF && node->levels > 0) {
+    while (node->forward[node->levels - 1] == OSET_EOF && node->levels - 1 > 0) {
         node->levels--;
     }
 
@@ -378,7 +388,113 @@ OrderedSet oset_split(OrderedSet oset, void* split_key) {
     return split;
 }
 
-OrderedSet oset_merge(OrderedSet a, OrderedSet b) { return OSET_ERROR; }
+OrderedSet oset_merge(OrderedSet a, OrderedSet b) {
+    assert(a != b);
+
+    // Hold the last nodes on each level of MERGED.
+    Vector last_nodes = vector_create(max(a->max_level, b->max_level), NULL);
+
+    // Create new Ordered Set.
+    OrderedSet merged = oset_create(a->compare, a->destroy_key, a->destroy_value);
+    if (a == OSET_ERROR) return OSET_ERROR;
+
+    // Initialize new Ordered Set.
+    if (merged->max_level < max(a->max_level, b->max_level)) {
+        // Update max_level to reallocate the header node.
+        merged->max_level = max(a->max_level, b->max_level);
+
+        // Reallocate and initialize to NULL the forward array of MERGED header node.
+        free(merged->header->forward);
+        merged->header->forward =
+            calloc(merged->max_level * sizeof(*merged->header->forward), merged->max_level);
+        if (merged->header->forward == NULL) return OSET_ERROR;
+    }
+    merged->capacity = a->capacity > b->capacity ? a->capacity : b->capacity;
+    merged->header->levels = max(a->header->levels, b->header->levels);
+
+    // Initialize last_nodes vector.
+    for (int i = 0; i < merged->header->levels; i++) vector_set_at(last_nodes, i, merged->header);
+
+    // Transfer nodes from A and B to MERGED.
+    while (a->header->forward[0] != OSET_EOF && b->header->forward[0] != OSET_EOF) {
+        void* key1 = a->header->forward[0]->key;
+        void* key2 = b->header->forward[0]->key;
+
+        if (merged->compare(key1, key2) > 0) {
+            // Exchange, key1 and key2, and Ordered Sets A and B.
+
+            void* temp_key = key1;
+            key1 = key2;
+            key2 = temp_key;
+
+            OrderedSet temp_oset = a;
+            a = b;
+            b = temp_oset;
+        }
+
+        // Add elements to MERGED from A with keys <= key2.
+        int lvl = 0;
+        do {
+            OrderedSetNode node = vector_get_at(last_nodes, lvl);
+            node->forward[lvl] = a->header->forward[lvl];
+            if (lvl == 0 && node->forward[0] != OSET_EOF) node->forward[0]->previous = node;
+            lvl++;
+        } while (
+            lvl < merged->header->levels &&
+            a->header->forward[lvl] != OSET_EOF &&
+            merged->compare(a->header->forward[lvl]->key, key2) < 0);
+        lvl--;
+
+        // For each level attached to MERGED, find endpoint at that level and remove it from A.
+        OrderedSetNode node = a->header->forward[lvl];
+        for (int i = lvl; i >= 0; i--) {
+            while (
+                node->forward[i] != OSET_EOF &&
+                merged->compare(node->forward[i]->key, key2) <= 0) {
+                node = node->forward[i];
+            }
+            vector_set_at(last_nodes, i, node);
+            a->header->forward[i] = node->forward[i];
+            if (i == 0 && node->forward[i] != OSET_EOF) {
+                node->forward[i]->previous = a->header;
+            }
+        }
+    }
+
+    // Connect left over nodes to MERGED.
+    OrderedSet left_over = b->header->forward[0] == OSET_EOF ? a : b;
+    for (int i = 0; i < left_over->header->levels; i++) {
+        OrderedSetNode node = vector_get_at(last_nodes, i);
+        node->forward[i] = left_over->header->forward[i];
+        if (i == 0 && node->forward[i] != OSET_EOF) {
+            node->forward[i]->previous = node;
+        }
+
+        // Traverse remaining nodes to find last node.
+        while (node->forward[i] != OSET_EOF) {
+            node = node->forward[i];
+        }
+        vector_set_at(last_nodes, i, node);
+
+        // Disconnect B header from left over nodes.
+        b->header->forward[i] = OSET_EOF;
+    }
+
+    // Update first pointer.
+    merged->first = merged->header->forward[0];
+
+    // Update last pointer.
+    merged->last = vector_get_at(last_nodes, 0);
+
+    // Update size.
+    merged->size = a->size + b->size;
+
+    oset_destroy(a);
+    oset_destroy(b);
+    vector_destroy(last_nodes);
+
+    return merged;
+}
 
 void oset_concat(OrderedSet a, OrderedSet b) {
     assert(a != b);
@@ -402,6 +518,10 @@ void oset_concat(OrderedSet a, OrderedSet b) {
             node->forward[i] = b->header->forward[i];
         }
     }
+
+    // Connect previous pointer of first node of B to last node of A to be able to traverse A in
+    // descending order.
+    if (b->first != NULL) b->first->previous = a->last;
 
     // Update last pointer.
     a->last = b->last;
