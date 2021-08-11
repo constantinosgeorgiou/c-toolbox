6380921de1b68cbf23d4a625303e77b3204a8f3f
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-08-11 19:50:33 +0300

    Add stack_push.

--- tests/stack_test.c
+++ tests/stack_test.c
@@ -4,20 +4,38 @@
 #include "stdlib.h"
 
 void test_create(void) {
-  Stack stack = stack_create(free);
+    Stack stack = stack_create(free);
 
-  TEST_CHECK(stack != NULL);
-  TEST_CHECK(stack_size(stack) == 0);
-  TEST_CHECK(stack_is_empty(stack) == true);
+    TEST_CHECK(stack != NULL);
+    TEST_CHECK(stack_size(stack) == 0);
+    TEST_CHECK(stack_is_empty(stack) == true);
 
-  DestroyFunc destroy_value = stack_set_destroy_value(stack, NULL);
-  TEST_CHECK(destroy_value == free);
+    DestroyFunc destroy_value = stack_set_destroy_value(stack, NULL);
+    TEST_CHECK(destroy_value == free);
 
-  stack_destroy(stack);
+    stack_destroy(stack);
+}
+
+void test_push(void) {
+    Stack stack = stack_create(free);
+
+    int N = 1000;
+    int* array = malloc(N * sizeof(*array));
+
+    for (int i = 0; i < 1000; i++) {
+        stack_insert_top(stack, &array[i]);
+        TEST_CHECK(stack_size(stack) == (i++));
+        TEST_CHECK(stack_peek(stack) == &array[i]);
+        TEST_CHECK(stack_is_empty(stack) == false);
+    }
+
+    stack_destroy(stack);
+    free(array);
 }
 
 TEST_LIST = {
     {"stack_create", test_create},
+    {"stack_push", test_push},
 
     {NULL, NULL}  // End of tests.
 };
