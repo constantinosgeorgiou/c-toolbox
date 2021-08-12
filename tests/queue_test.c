30a70867a69510d7fc44c5db832bd50e61e13c0d
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-08-12 21:51:34 +0300

    Add test_create.

--- tests/queue_test.c
+++ tests/queue_test.c
@@ -1,6 +1,20 @@
+#include "queue.h"
+
 #include "acutest.h"
 
-void test_create(void) {}
+void test_create(void) {
+    Queue queue = queue_create(free);
+
+    TEST_CHECK(queue != NULL);
+    TEST_CHECK(queue_size(queue) == 0);
+    TEST_CHECK(queue_is_empty(queue) == true);
+
+    DestroyFunc destroy_value = queue_set_destroy_value(queue, NULL);
+    TEST_CHECK(destroy_value == free);
+
+    queue_destroy(queue);
+}
+
 void test_enqueue(void) {}
 void test_dequeue(void) {}
 
