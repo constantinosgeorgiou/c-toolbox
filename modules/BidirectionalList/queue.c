1e82df4bdab708e27393ac9688b63e243e74ad2d
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-08-12 22:01:40 +0300

    queue_create: Add implementation.

--- modules/BidirectionalList/queue.c
+++ modules/BidirectionalList/queue.c
@@ -5,3 +5,13 @@
 struct queue {
     BList data;
 };
+
+Queue queue_create(DestroyFunc destroy_value) {
+    Queue queue = malloc(sizeof(*queue));
+    if (queue == NULL) return NULL;
+
+    queue->data = blist_create(destroy_value);
+    if (queue->data == NULL) return NULL;
+
+    return queue;
+}
