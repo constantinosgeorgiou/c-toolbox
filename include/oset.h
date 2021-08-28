4538ecf25aafd80ca9450a0fff6e4bdb3bf246f7
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-08-28 15:26:22 +0300

    Improve documentation
    Add grouping

--- include/oset.h
+++ include/oset.h
@@ -7,179 +7,222 @@
 /// The user does not need to know how an Ordered Set is implemented, they use
 /// the API functions provided `oset_<operation>` with the appropriate
 /// parameters.
+
+#ifndef OSET_H
+#define OSET_H
+
+#include "common_types.h" // CompareFunc, DestroyFunc
+#include <stdbool.h>      // bool
+#include <stddef.h>       // size_t
+
+/// OrderedSet type.
 ///
-/// Operations supported:
-/// - oset_create(): Creates and returns an ordered set.
-/// - oset_destroy(): Frees all the memory allocated by an ordered set.
-/// - oset_size(): Returns the number of elements in an ordered set.
-/// - oset_insert(): Associates a key with a value.
-/// - oset_remove(): Removes a key.
-/// - oset_find(): Finds the value associated with a key.
-/// - oset_find_node(): Finds the node of the value associated with
-/// a key.
-/// - oset_split(): Removes all elements with keys >= split_key and returns them
-/// in a new ordered set.
-/// - oset_merge(): Creates an ordered set containing the elements
-/// of specified ordered sets.
-/// - oset_concat(): Appends ordered set B to the end of ordered set A.
-/// - oset_node_key(): Returns key of a node.
-/// - oset_node_value(): Returns value of a node.
-/// - oset_first(): Returns the first node of an ordered set.
-/// - oset_last(): Returns the last node of an ordered set.
-/// - oset_next(): Returns the next node of a node.
-/// - oset_previous(): Returns the previous node of a node.
-/// - oset_set_destroy_key(): Changes function called each time a key is
-/// removed.
-/// - oset_set_destroy_value(): Changes function called each time a value is
-/// removed.
-
-#pragma once
-
-#include "common_types.h"
-
-#define OSET_ERROR (OrderedSet)0
-
-#include <stdbool.h> // bool
-#include <stddef.h>  // size_t
-
+/// Incomplete struct, to keep it implementation independent.
+///
+/// The user does not need to know how an OrderedSet is implemented, they use
+/// the API functions provided `oset_<operation>` with the appropriate
+/// parameters.
 typedef struct ordered_set *OrderedSet;
-typedef struct ordered_set_node *OrderedSetNode;
 
-/// @brief Creates and returns an Ordered Set.
+/// Allocate space for a new ordered set.
 ///
-/// @param compare Compares two elements.
-/// @param destroy_key If destroy_key != NULL, call destroy_key(key) each time a
-/// key is removed.
-/// @param destroy_value If destroy_value != NULL, call destroy_value(value)
-/// each time a value is
-///                      removed.
+/// \p compare , compares two elements \p a and \p b :
+/// - If \p a < \p b , return number < 0.
+/// - If \p a > \p b , return number > 0.
+/// - If \p a equivalent to \p b , return 0.
 ///
-/// @return Newly created Ordered Set, or OSET_ERROR, if failed to allocate
-/// enough memory.
+/// If \p destroy_key is not NULL, then when an element gets removed,
+/// `destroy_key(key)` is called to deallocate the space held by key.
 ///
+/// If \p destroy_value is not NULL, then when an element gets removed,
+/// `destroy_value(value)` is called to deallocate the space held by value.
+///
+/// \param compare Compares two elements. \sa CompareFunc.
+/// \param destroy_key When an element gets removed, `destroy_key(key)` is
+/// called, if not NULL, to deallocate the space held by key.
+/// \param destroy_value When an element gets removed, `destroy_value(value)` is
+/// called, if not NULL, to deallocate the space held by value.
+///
+/// \return Newly created ordered set, or NULL if an error occured.
 OrderedSet oset_create(CompareFunc compare, DestroyFunc destroy_key,
                        DestroyFunc destroy_value);
 
-/// @brief Frees all the memory allocated by given Ordered Set.
+/// Deallocate the space held by \p oset .
 ///
-/// Any operation on the Ordered Set after its destruction, results in undefined
-/// behaviour.
-///
+/// Any operation on \p oset after its destruction, causes undefined behaviour.
 void oset_destroy(OrderedSet oset);
 
-/// @brief Changes function called each time a key is removed to destroy_key.
+/// Changes the destroy function called on each key's removal/overwrite
+/// to \p destroy_key .
 ///
-/// @return Previous destroy_key function.
+/// Typical usage:
+/// \code {.c}
+///   int compare_ints(const void* a, const void* b){
+///     return *(int*)a - *(int*)b;
+///   }
 ///
+///   OrderedSet myoset = oset_create(compare_ints, free, NULL);
+///   DestroyFunc old = set_set_destroy_key(myoset, NULL);
+///   // old == free
+/// \endcode
+///
+/// \param destroy_key When an element gets removed, `destroy_key(key)` is
+/// called, if not NULL, to deallocate the space held by key.
+///
+/// \return Previous `destroy_key` function.
 DestroyFunc oset_set_destroy_key(OrderedSet oset, DestroyFunc destroy_key);
 
-/// @brief Changes function called each time a value is removed to
-/// destroy_value.
+/// Changes the destroy function called on each value's removal/overwrite
+/// to \p destroy_value .
 ///
-/// @return Previous destroy_value function.
+/// Typical usage:
+/// \code {.c}
+///   int compare_ints(const void* a, const void* b){
+///     return *(int*)a - *(int*)b;
+///   }
 ///
+///   OrderedSet myoset = oset_create(compare_ints, NULL, free);
+///   DestroyFunc old = oset_set_destroy_value(mymap, NULL);
+///   // old == free
+/// \endcode
+///
+/// \param destroy_value When an element gets removed, `destroy_value(value)` is
+/// called, if not NULL, to deallocate the space held by value.
+///
+/// \return Previous `destroy_value` function.
 DestroyFunc oset_set_destroy_value(OrderedSet oset, DestroyFunc destroy_value);
 
-/// @brief Returns the number of elements in the Ordered Set.
-///
-/// @return The number of elements in the Ordered Set.
-///
+/// Returns the number of elements in the \p oset .
 size_t oset_size(OrderedSet oset);
 
-/// @brief Associates a key with a value (inserting the key if not already
+/// Associates a key with a value (inserting the key if not already
 /// present). Duplicate keys are treated like a stack, Last In First Out.
 ///
 /// Keys can not be NULL.
 ///
+
+/// Associates \p key with \p value.
+///
+/// Insert \p key if not already present.
+///
+/// Duplicate keys are treated like a stack, *Last In First Out*.
+///
+/// \p key can not be `NULL`.
+///
+/// While a key is part of \p oset , any change to its content causes
+/// undefined behaviour.
 void oset_insert(OrderedSet oset, void *key, void *value);
 
-/// @brief Removes specified key.
-///        Duplicate keys are treated like a stack, Last In First Out.
+/// Remove \p key from \p oset .
 ///
-/// Keys can not be NULL.
+/// Duplicate keys are treated like a stack, *Last In First Out*.
 ///
-/// @return true, if key was removed successfully, otherwise false.
+/// \p key can not be `NULL`.
 ///
+/// \param oset
+/// \param key
+/// \return true, if \p key was removed successfully, otherwise false.
 bool oset_remove(OrderedSet oset, void *key);
 
-/// @brief Finds and returns the value associated with specified key.
-///        Duplicate keys are treated like a stack, Last In First Out.
+/// Find and return the value associated with \p key .
 ///
-/// Keys can not be NULL.
+/// Duplicate keys are treated like a stack, *Last In First Out*.
 ///
-/// @return Value associated with specified key, or NULL, if key not found.
+/// \p key can not be `NULL`.
 ///
+/// \return Value associated with \p key , or NULL if \p key is not part of
+/// \p oset .
 void *oset_find(OrderedSet oset, void *key);
 
-/// @brief Removes all elements with keys >= split_key and returns them in a new
-///        Ordered Set.
+/// Remove all elements with keys >= \p split_key and return them in a new
+/// ordered set.
 ///
-/// split_key can not be NULL. split_key < smallest_key transfers all elements
-/// to newly created Ordered Set. split_key > largest_key returns an empty
-/// Ordered Set. Splitting an empty Ordered Set results in an error.
+/// \p split_key can not be NULL.
 ///
-/// @return Newly created Ordered Set with keys >= split_key, or OSET_ERROR, if
-/// an error occurred.
+/// if \p split_key < smallest_key, transfer all elements to newly created
+/// ordered set.
 ///
+/// if \p split_key > largest_key, return an empty ordered set.
+///
+/// Splitting an empty ordered set results in an error.
+///
+/// @return Newly created Ordered Set with keys >= \p split_key, or NULL, if an
+/// error occurred.
 OrderedSet oset_split(OrderedSet oset, void *split_key);
 
-/// @brief Creates and returns a new Ordered Set containing the elements of
-/// Ordered Set a and
-///        Ordered Set b.
+/// Create and return a new ordered set containing the elements of \p a and
+/// \p b .
 ///
-/// If an element appears in both Ordered Sets, use the value field from Ordered
-/// Set  b  . Any operation on the Ordered Sets  a  and  b  after they are
-/// merged results in undefined behaviour. a == b, results in undefined
+/// If an element appears in both ordered sets, use the value field from \p b .
+///
+/// If \p a is \p b , it causes undefined behaviour.
+///
+/// Any operation on \p a and \p b after they are merged causes undefined
 /// behaviour.
-///
 OrderedSet oset_merge(OrderedSet a, OrderedSet b);
 
-/// @brief Appends B to the end of A.
+/// Append \p b to the end of \p a .
 ///
-/// last_key in a > first_key in b, results in undefined behaviour. a == b,
-/// results in undefined behaviour. Any operation on  b  after it is merged with
-/// a  results in undefined behaviour.
+/// If last_key in \p a > first_key in \p b , it causes undefined behaviour.
 ///
+/// If \p a is \p b , it causes undefined behaviour.
+///
+/// Any operation on \p b after it is merged with \p a causes undefined
+/// behaviour.
 void oset_concat(OrderedSet a, OrderedSet b);
 
-#define OSET_BOF                                                               \
-    (OrderedSetNode)0 // Defines the virtual beginning of the Ordered Set.
-#define OSET_EOF                                                               \
-    (OrderedSetNode)0 // Defines the virtual end of the Ordered Set.
+/// OrderedSetNode type.
+///
+/// Incomplete struct, to keep it implementation independent.
+///
+/// The user does not need to know how a OrderedSetNode is implemented, they use
+/// the API functions provided `oset_<operation>_node` and
+/// `oset_node_<operation>` with the appropriate parameters.
+typedef struct ordered_set_node *OrderedSetNode;
 
-/// @brief Finds and returns the node of the value associated with specified
-/// key.
-///        Duplicate keys are treated like a stack, Last In First Out.
+/// Defines the virtual beginning of the Ordered Set.
+#define OSET_BOF (OrderedSetNode)0
+
+/// Defines the virtual end of the Ordered Set.
+#define OSET_EOF (OrderedSetNode)0
+
+/// Find and return the node associated with \p key .
 ///
-/// Keys can not be NULL.
+/// Duplicate keys are treated like a stack, *Last In First Out*.
 ///
-/// @return Node of the value associated with specified key, or OSET_EOF, if key
-/// not found.
+/// \p key can not be `NULL`.
 ///
+/// \return Value associated with \p key , or NULL if \p key is not part of
+/// \p oset .
 OrderedSetNode oset_find_node(OrderedSet oset, void *key);
 
-/// @brief Returns key of specified node.
+/// Return the key of \p node .
 ///
+/// If \p node is `NULL`, it causes to undefined behaviour.
 void *oset_node_key(OrderedSet oset, OrderedSetNode node);
 
-/// @brief Returns value of specified node.
+/// Return the value of \p node .
 ///
+/// If \p node is `NULL`, it causes to undefined behaviour.
 void *oset_node_value(OrderedSet oset, OrderedSetNode node);
 
-/// @brief Returns the first node of the Ordered Set, or SET_EOF, if Ordered set
-/// is empty.
-///
+/// \defgroup traversal Traversal functions
+///@{
+
+/// Return first node, or `SET_BOF` if \p oset is empty.
 OrderedSetNode oset_first(OrderedSet oset);
 
-/// @brief Returns the last node of the Ordered Set, or SET_EOF, if Ordered set
-/// is empty.
-///
+/// Return last node, or `OSET_EOF` if \p oset is empty.
 OrderedSetNode oset_last(OrderedSet oset);
 
-/// @brief Returns the next node of node, or OSET_EOF, if node is last.
-///
+/// Return the next node of \p node , or `OSET_EOF` if \p node is the last node
+/// in \p oset .
 OrderedSetNode oset_next(OrderedSet oset, OrderedSetNode node);
 
-/// @brief Returns the previous node of node, or OSET_BOF, if node is first.
-///
+/// Return the previous node of \p node , or `OSET_BOF` if \p node is the first
+/// node in \p oset .
 OrderedSetNode oset_previous(OrderedSet oset, OrderedSetNode node);
+
+///@}  // End of traversal.
+
+#endif // OSET_H
