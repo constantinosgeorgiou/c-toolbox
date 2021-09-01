e955d80f4badb0921703e70e1c130a9a95b32d1e
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-09-01 21:58:39 +0300

    Refactor file.

--- modules/LinkedList/slist.c
+++ modules/LinkedList/slist.c
@@ -1,191 +1,169 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-///
-/// @file ADTList.c
-/// @author Constantinos Georgiou
-/// @brief Implementation of ADTList interface via Linked List.
-/// @version 1
-///
-////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "slist.h"
 
-#include "ADTList.h"
+#include <assert.h> // assert
+#include <stdlib.h> // size_t, sizeof, malloc, free
 
-#include <assert.h>
-#include <stdlib.h>
-
-struct list {
-    ListNode sentinel;
-    ListNode last;
-    int size;
-    DestroyFunc destroy;
+struct slist {
+  SListNode sentinel;
+  SListNode last;
+  int size;
+  DestroyFunc destroy_value;
 };
 
-struct list_node {
-    ListNode next;
-    void* value;
+struct slist_node {
+  SListNode next;
+  void *value;
 };
 
-List list_create(DestroyFunc destroy) {
-    List list = malloc(sizeof(*list));
-    list->size = 0;
-    list->destroy = destroy;
-
-    list->sentinel = malloc(sizeof(*list->sentinel));
-    if (list->sentinel == NULL) {
-        free(list);
-        return NULL;
-    }
-    list->sentinel->next = NULL;
-
-    list->last = list->sentinel;
-
-    return list;
-}
-
-void list_destroy(List list) {
-    ListNode node = list->sentinel;
-    while (node != NULL) {
-        ListNode next = node->next;
-
-        if (node != list->sentinel && list->destroy != NULL) {
-            list->destroy(node->value);
-        }
-
-        free(node);
-        node = next;
-    }
-
-    free(list);
-}
-
-DestroyFunc list_set_destroy_value(List list, DestroyFunc destroy) {
-    DestroyFunc old = list->destroy;
-    list->destroy = destroy;
-    return old;
-}
-
-int list_size(List list) {
-    return list->size;
-}
-
-ListNode list_insert_next(List list, ListNode node, void* value) {
-    if (node == LIST_BOF) {  // Insert node next to sentinel node.
-        node = list->sentinel;
-    }
-
-    ListNode new = malloc(sizeof(*new));
-    if (new == NULL) {
-        return NULL;
-    }
-    new->value = value;
-
-    new->next = node->next;
-    node->next = new;
-
-    if (list->last == node) {
-        list->last = new;
-    }
-
-    list->size += 1;
-
-    return new;
-}
-
-ListNode list_remove_next(List list, ListNode node) {
-    if (node == LIST_BOF) {  // Remove node next to sentinel node.
-        node = list->sentinel;
-    }
-
-    ListNode removed = node->next;
-    if (removed == NULL) {
-        return node;
-    }
-
-    if (list->destroy != NULL) {
-        list->destroy(removed->value);
-    }
-
-    node->next = removed->next;
-
-    free(removed);
-
-    if (list->last == removed) {
-        list->last = node;
-    }
-
-    list->size -= 1;
-
-    return node;
-}
-
-List list_concatenate(List a, List b) {
-    assert(a != b);
-
-    a->last->next = b->sentinel->next;
-
-    // Update last pointer and size:
-    a->last = b->last;
-    a->size += b->size;
-
-    // Isolate sentinel node and destroy b list:
-    b->sentinel->next = NULL;
-    list_destroy(b);
-
-    return a;
-}
-
-ListNode list_find_node(List list, void* value, CompareFunc compare) {
-    for (ListNode node = list->sentinel->next; node != NULL; node = node->next) {
-        if (compare(value, node->value) == 0) {
-            return node;  // FOUND
-        }
-    }
-
-    return LIST_EOF;  // NOT FOUND
-}
-
-void* list_find(List list, void* value, CompareFunc compare) {
-    ListNode node = list_find_node(list, value, compare);
-    return node == NULL ? NULL : node->value;
-}
-
-void* list_node_value(List list, ListNode node) {
-    assert(node != NULL);
-    return node->value;
-}
-
-////////////    TRAVERSAL    ///////////////////////////////////////////////////////////////////////
-
-ListNode list_first(List list) {
-    return list->sentinel->next;
-}
-
-ListNode list_last(List list) {
-    if (list->last == list->sentinel) {
-        return LIST_EOF;
-    } else {
-        return list->last;
-    }
-}
-
-ListNode list_next(List list, ListNode node) {
-    assert(node != NULL);
-    return node->next;
-}
-
-void* list_get_at(List list, int position) {
-    if (position < 0 || position > list->size) {
-        return NULL;  // Out of bounds
-    }
-
-    ListNode node = list->sentinel->next;
-
-    // Traverse list:
-    for (int index = 0; index < list->size; index++) {
-        if (index == position) {
-            return node->value;
-        }
-        node = node->next;
-    }
-
+SList slist_create(DestroyFunc destroy_value) {
+  SList slist = malloc(sizeof(*slist));
+  if (slist == NULL)
     return NULL;
+
+  slist->sentinel = malloc(sizeof(*slist->sentinel));
+  if (slist->sentinel == NULL) {
+    free(slist);
+    return NULL;
+  }
+  slist->sentinel->next = NULL;
+
+  slist->size = 0;
+  slist->destroy_value = destroy_value;
+
+  slist->last = slist->sentinel;
+
+  return slist;
+}
+
+void slist_destroy(SList slist) {
+  SListNode node = slist->sentinel;
+  while (node != NULL) {
+    SListNode next = node->next;
+
+    if (node != slist->sentinel && slist->destroy_value != NULL) {
+      slist->destroy_value(node->value);
+    }
+
+    free(node);
+    node = next;
+  }
+
+  free(slist);
+}
+
+DestroyFunc slist_set_destroy_value(SList slist, DestroyFunc destroy_value) {
+  DestroyFunc old = slist->destroy_value;
+  slist->destroy_value = destroy_value;
+  return old;
+}
+
+size_t slist_size(SList slist) { return slist->size; }
+
+void slist_insert_next(SList slist, SListNode node, void *value) {
+  if (node == SLIST_BOF)
+    node = slist->sentinel; // Insert node next to sentinel node.
+
+  SListNode new_node = malloc(sizeof(*new_node));
+  if (new_node == NULL)
+    return;
+
+  new_node->value = value;
+
+  new_node->next = node->next;
+  node->next = new_node;
+
+  if (slist->last == node)
+    slist->last = new_node;
+
+  slist->size += 1;
+}
+
+void slist_remove_next(SList slist, SListNode node) {
+  if (node == SLIST_BOF)
+    node = slist->sentinel; // Remove node next to sentinel node.
+
+  SListNode removed = node->next;
+  if (removed == NULL)
+    return;
+
+  if (slist->destroy_value != NULL)
+    slist->destroy_value(removed->value);
+
+  node->next = removed->next;
+
+  free(removed);
+
+  if (slist->last == removed)
+    slist->last = node;
+
+  slist->size -= 1;
+}
+
+SList slist_concat(SList dest, SList src) {
+  assert(dest != src && "slist_concat: slists given are the same.");
+
+  dest->last->next = src->sentinel->next;
+
+  // Update last pointer and size:
+  dest->last = src->last;
+  dest->size += src->size;
+
+  // Isolate sentinel node and destroy src slist:
+  src->sentinel->next = NULL;
+  slist_destroy(src);
+
+  return dest;
+}
+
+SListNode slist_find_node(SList slist, void *value, CompareFunc compare) {
+  for (SListNode node = slist->sentinel->next; node != NULL;
+       node = node->next) {
+    if (compare(value, node->value) == 0)
+      return node; // FOUND
+  }
+
+  return SLIST_EOF; // NOT FOUND
+}
+
+void *slist_find(SList slist, void *value, CompareFunc compare) {
+  SListNode node = slist_find_node(slist, value, compare);
+  return node == NULL ? NULL : node->value;
+}
+
+void *slist_node_value(SList slist, SListNode node) {
+  assert(node != NULL);
+  return node->value;
+}
+
+SListNode slist_first(SList slist) { return slist->sentinel->next; }
+
+SListNode slist_last(SList slist) {
+  if (slist->last == slist->sentinel)
+    return SLIST_EOF;
+  else
+    return slist->last;
+}
+
+SListNode slist_next(SList slist, SListNode node) {
+  assert(node != NULL);
+  return node->next;
+}
+
+void *list_get_at(SList slist, int pos) {
+  if (pos < 0)
+    pos = 0;
+  if (pos > slist->size)
+    pos = slist->size;
+
+  SListNode node = slist->sentinel->next;
+
+  // Traverse slist:
+  for (int index = 0; index < slist->size; index++) {
+    if (index == pos)
+      return node->value;
+
+    node = node->next;
+  }
+
+  return NULL;
 }
